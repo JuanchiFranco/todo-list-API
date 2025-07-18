babb48f4549565c1389417e210f7fb5d762c1e7f
Juan Franco Vargas <francovargasjuandario@gmail.com>
2025-07-18 17:17:11 -0500

    rate limit and slow down added to server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
+import { slowDown } from 'express-slow-down';
 import usersRoutes from './routes/users.routes.js';
 import todosRoutes from './routes/todos.routes.js';
 import 'dotenv/config';
@@ -11,6 +13,23 @@ async function startServer() {
 
         app.use(express.json());
         app.use(express.static('public'));
+
+        const limiter = rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100, // Limit each IP to 100 requests per windowMs
+            message: 'Muchas solicitudes desde esta IP, por favor intente de nuevo más tarde.',
+            standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+            legacyHeaders: false // Disable the `X-RateLimit-*` headers
+        });
+
+        const speedLimiter = slowDown({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            delayAfter: 100, // Begin delaying requests after 100 requests
+            delayMs: (hits) => hits * 500, // Delay each request by 500ms
+        });
+
+        app.use(limiter);
+        app.use(speedLimiter);
 
         app.get('/', (req, res) => {
             res.sendFile('index.html', { root: 'public' });
